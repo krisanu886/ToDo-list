ba21e0b8190a0150820b2851f24740279d949406
Krisanu <kisanudey886@gmail.com>
2022-02-10 11:35:39 +0530

    updatedToDo-List

--- src/ToDoList.js
+++ src/ToDoList.js
@@ -1,4 +1,7 @@
 import React, { useState } from "react";
+import { FcFullTrash } from "react-icons/fc";
+// import { FcDownload } from "react-icons/fc";
+import { FaPencilAlt } from "react-icons/fa";
 import "./style.css";
 
 const ToDoList = () => {
@@ -13,24 +16,40 @@ const ToDoList = () => {
     });
     setInputList("");
   };
+  const textDelete=()=>{}
+  const textEdit=()=>{}
   return (
     <>
       <div className="container">
-        <div className="container2">
-          <h1>ToDo List</h1>
-          <input
-            type="text"
-            value={inputList}
-            placeholder="Add Your Items"
-            onChange={inputItems}
-          />
-          <button onClick={itemSubmit}>+</button>
+        <h1 className="h1">ToDo List</h1>
+        <input
+          type="text"
+          value={inputList}
+          placeholder="Add Your Items"
+          onChange={inputItems}
+        />
+        <button onClick={itemSubmit}>+ </button>
 
-          {/* Hello brother */}
-          {items.map((itemsValue) => {
-            return <li>{itemsValue}</li>;
-          })}
-        </div>
+        {items.map((itemsValue) => {
+          return (
+            <div className="map">
+              <textArea type="text" value={inputList}>
+                {itemsValue}
+              </textArea>
+              <div className="mapButton">
+                <i onClick={textDelete}>
+                  <FcFullTrash />
+                </i>
+                {/* <i onClick={textDownload}>
+                  <FcDownload />
+                </i> */}
+                <i onClick={textEdit}>
+                  <FaPencilAlt />
+                </i>
+              </div>
+            </div>
+          );
+        })}
       </div>
     </>
   );
